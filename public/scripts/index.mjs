3ce63fb1fbe6c1322b8593b7ce522e5f98e9649d
Henrik Forsberg <henrik.forsberg1990@gmail.com>
2024-09-23 14:38:20 +0200

    feat: added search

--- public/scripts/index.mjs
+++ public/scripts/index.mjs
@@ -57,6 +57,65 @@ const setUpAllPrimaryAreas = () => {
   });
 };
 
+const resetAllPrimaryAreas = () => {
+  const allPrimaryAreas = document.getElementById("allPrimaryAreas");
+
+  for (var i = 1; i < allPrimaryAreas.rows.length; ) {
+    allPrimaryAreas.deleteRow(i);
+  }
+
+  data.forEach((value) => {
+    const template = `
+    <tr style="background-color: ${value.index_classification.color}; color: white">
+         <td>${value.area}</td>
+         <td>${value.index_classification.status}</td>
+         <td>${value.index}</td>
+       </tr>
+   `;
+    allPrimaryAreas.innerHTML += template;
+  });
+};
+
+const handleSearchPrimaryAreas = (searchText) => {
+  if (!searchText) return setUpAllPrimaryAreas();
+
+  if (searchText.length <= 3) return;
+
+  const allPrimaryAreas = document.getElementById("allPrimaryAreas");
+
+  for (var i = 1; i < allPrimaryAreas.rows.length; ) {
+    allPrimaryAreas.deleteRow(i);
+  }
+
+  const filtered = areas
+    .filter(
+      (a) =>
+        a.address.toLowerCase().includes(searchText.toLowerCase()) ||
+        a.primary_area.toLowerCase().includes(searchText.toLowerCase())
+    )
+    .reduce((agg, current) => {
+      if (!agg) return [current.primary_area];
+
+      if (agg.includes(current.primary_area)) return agg;
+
+      return [...agg, current.primary_area];
+    }, []);
+
+  const selectedData = filtered?.map((f) => data.find((d) => d.area === f));
+
+  selectedData?.forEach((value) => {
+    const template = `
+    <tr style="background-color: ${value.index_classification.color}; color: white">
+         <td>${value.area}</td>
+         <td>${value.index_classification.status}</td>
+         <td>${value.index}</td>
+       </tr>
+   `;
+
+    allPrimaryAreas.innerHTML += template;
+  });
+};
+
 const sortTable = (index, comparer, event) => {
   let i = 0;
   let shouldSwitch = false;
@@ -66,13 +125,6 @@ const sortTable = (index, comparer, event) => {
   const ascending = "asc";
   const descending = "desc";
   let dir = descending;
-
-  // const th = table.getElementsByTagName("TH")[index];
-
-  // const className =
-  //   th.className === "headerSortDown" ? "headerSortUp" : "headerSortDown";
-
-  // th.className = className;
 
   setSortArrow(table, index);
 
@@ -134,3 +186,11 @@ document
 document
   .getElementById("table-header-area")
   .addEventListener("click", () => sortTable(0, "area"));
+
+document
+  .getElementById("searchText")
+  .addEventListener("input", (e) =>
+    e?.target?.value === ""
+      ? resetAllPrimaryAreas()
+      : handleSearchPrimaryAreas(e?.target?.value)
+  );
