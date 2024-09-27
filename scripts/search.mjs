6b41466472aed741dca43ecd4d8f951e98caebf5
Henrik Forsberg <henrik.forsberg1990@gmail.com>
2024-09-27 21:05:11 +0200

    Delete scripts/search.mjs

--- scripts/search.mjs
+++
@@ -1,7 +0,0 @@
-import data from "../data/primary-area.json" with {"type": "json"};
-
-const searchTerm = "Banjo";
-
-const t = data.filter(d => d.address.includes(searchTerm));
-
-console.log({t});
