2a93f5712b3a84de6b7f0c6d6e9f7b216fad84b9
Henrik Forsberg <henrik.forsberg1990@gmail.com>
2024-11-07 17:47:39 +0100

    feat: regenerated html

--- pipelines/gbpi/templates/html.mjs
+++ pipelines/gbpi/templates/html.mjs
@@ -7,7 +7,7 @@ export const htmlTemplate = (
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Göteborgs Boendebarometer</title>
+    <title>${indexShortTitle}</title>
     <link rel="icon" type="image/png" href="images/home.png" />
     <link rel="stylesheet" href="styles/index.css" />
   </head>
